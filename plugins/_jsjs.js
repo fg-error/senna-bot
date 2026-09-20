d866669210fa01991498a6e0582d61ab859394f8
fg-error <fg98ff@gmail.com>
2026-09-20 00:13:50 -0300

    Aura --- laura

--- plugins/_jsjs.js
+++ plugins/_jsjs.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 let handler = async (m, { conn }) => {
   const img = (await axios.get('https://raw.githubusercontent.com/fg-error/fg-team/refs/heads/main/img/hu.json')).data
-  const url = pickRandom(img)
+  const url = pickRandom(img) 
 
   const stiker = await sticker(null, url, global.packname, global.author)
   await conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, false, { asSticker: true })
